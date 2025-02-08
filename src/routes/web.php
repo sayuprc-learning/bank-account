1fd1280717aab51d2ce877093a7bcd4fa6c8aa39
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 15:00:01 +0900

    Web ルーティングを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\BankAccount\Web\ListController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list', [ListController::class, 'handle']);

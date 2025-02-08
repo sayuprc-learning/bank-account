2eaa5faad0b9eb2587a157fc5f31b11ce3f9895b
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 14:43:22 +0900

    一覧取得ユースケースの実装を作成

---
+++ src/packages/BankAccount/Applications/List/ListInteractor.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BankAccount\Applications\List;
+
+use BankAccount\Domain\BankAccountRepositoryInterface;
+use BankAccount\UseCases\List\ListResponse;
+use BankAccount\UseCases\List\ListUseCaseInterface;
+
+class ListInteractor implements ListUseCaseInterface
+{
+    public function __construct(
+        private readonly BankAccountRepositoryInterface $bankAccountRepository,
+    ) {
+    }
+
+    public function handle(): ListResponse
+    {
+        return new ListResponse($this->bankAccountRepository->all());
+    }
+}

455669f0b7aa8a594f0ca49f0873b32712061344
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 15:06:09 +0900

    口座振込の Web 用 User Interface の実装を作成

---
+++ src/resources/views/bank_accounts/transfer.blade.php
@@ -0,0 +1,7 @@
+<form action="/transfer" method="post">
+    @csrf
+    振込元口座番号: <input type="text" name="from_account_number"><br>
+    振込先口座番号: <input type="text" name="to_account_number"><br>
+    振込金額: <input type="number" name="amount"><br>
+    <input type="submit" value="振込">
+</form>

3cba1f99b6431177e93d83bb47c8e6e9d0237e33
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 15:06:32 +0900

    Web 用ルーティングを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\BankAccount\Web\ListController;
+use App\Http\Controllers\BankAccount\Web\TransferController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,6 @@ Route::get('/', function () {
 });
 
 Route::get('/list', [ListController::class, 'handle']);
+
+Route::get('/transfer', [TransferController::class, 'view']);
+Route::post('/transfer', [TransferController::class, 'handle']);

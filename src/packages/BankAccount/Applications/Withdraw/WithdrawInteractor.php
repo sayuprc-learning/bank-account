fb8a4f01474c282388a0434a5b3d3aadc8d8f23d
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 16:37:58 +0900

    引き落としユースケースの実装を作成

---
+++ src/packages/BankAccount/Applications/Withdraw/WithdrawInteractor.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BankAccount\Applications\Withdraw;
+
+use BankAccount\Domain\AccountNumber;
+use BankAccount\Domain\BankAccountRepositoryInterface;
+use BankAccount\Domain\Money;
+use BankAccount\UseCases\Withdraw\WithdrawRequest;
+use BankAccount\UseCases\Withdraw\WithdrawResponse;
+use BankAccount\UseCases\Withdraw\WithdrawUseCaseInterface;
+use Exception;
+use Shared\Transaction\TransactionInterface;
+
+class WithdrawInteractor implements WithdrawUseCaseInterface
+{
+    public function __construct(
+        private readonly TransactionInterface $scope,
+        private readonly BankAccountRepositoryInterface $bankAccountRepository,
+    ) {
+    }
+
+    public function handle(WithdrawRequest $request): WithdrawResponse
+    {
+        $result = $this->scope->transaction(function () use ($request) {
+            if ($request->amount < 1) {
+                throw new Exception('引き落とし額は 1 以上の必要があります');
+            }
+
+            if (is_null($bankAccount = $this->bankAccountRepository->find(new AccountNumber($request->accountNumber)))) {
+                throw new Exception('引き落とし先口座が見つかりませんでした');
+            }
+
+            $amount = new Money($request->amount);
+
+            $bankAccount->withdraw($amount);
+
+            $this->bankAccountRepository->save($bankAccount);
+
+            return $bankAccount->balance->value;
+        });
+
+        return new WithdrawResponse(
+            $request->accountNumber,
+            $request->amount,
+            $result,
+        );
+    }
+}

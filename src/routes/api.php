ffbee2044c974ddc20708cda1fd10c1b07de0d37
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 14:47:41 +0900

    API ルーティングを追加

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\BankAccount\Api\v1\ListController;
 use App\Http\Controllers\BankAccount\Api\v1\TransferController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/v1/list', [ListController::class, 'handle']);
 Route::post('/v1/transfer', [TransferController::class, 'handle']);

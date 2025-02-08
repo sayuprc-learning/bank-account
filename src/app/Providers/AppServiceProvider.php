159517b5a639becb29f4832ec52984ffecfa743e
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 14:48:12 +0900

    DI コンテナの設定を追加

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use BankAccount\Applications\List\ListInteractor;
 use BankAccount\Applications\Transfer\TransferInteractor;
 use BankAccount\Domain\BankAccountRepositoryInterface;
 use BankAccount\Infrastructure\FileBankAccountRepository;
+use BankAccount\UseCases\List\ListUseCaseInterface;
 use BankAccount\UseCases\Transfer\TransferUseCaseInterface;
 use Illuminate\Support\ServiceProvider;
 use Shared\DebugTransaction\NopTransaction;
@@ -21,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(TransactionInterface::class, NopTransaction::class);
 
+        $this->app->bind(ListUseCaseInterface::class, ListInteractor::class);
         $this->app->bind(TransferUseCaseInterface::class, TransferInteractor::class);
 
         $this->app->bind(BankAccountRepositoryInterface::class, FileBankAccountRepository::class);

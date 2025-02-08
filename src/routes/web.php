f3afffcecfb2131304801d30aa616d88d9b9cef5
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 17:30:36 +0900

    不要ファイルやコードを削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,10 +9,6 @@ use App\Http\Controllers\BankAccount\Web\TransferController;
 use App\Http\Controllers\BankAccount\Web\WithdrawController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/list', [ListController::class, 'handle']);
 
 Route::get('/transfer', [TransferController::class, 'view']);

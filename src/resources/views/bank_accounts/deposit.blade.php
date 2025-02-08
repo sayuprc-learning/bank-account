227521c4a7ff458283867be575bd66f8bed2fc8e
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 17:09:13 +0900

    入金の Web 用 User Interface の実装を作成

---
+++ src/resources/views/bank_accounts/deposit.blade.php
@@ -0,0 +1,6 @@
+<form action="/deposit" method="post">
+    @csrf
+    入金先口座番号: <input type="text" name="account_number"><br>
+    入金額: <input type="number" name="amount"><br>
+    <input type="submit" value="入金">
+</form>
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\BankAccount\Web\CreateAccountController;
+use App\Http\Controllers\BankAccount\Web\DepositController;
 use App\Http\Controllers\BankAccount\Web\ListController;
 use App\Http\Controllers\BankAccount\Web\TransferController;
 use Illuminate\Support\Facades\Route;
@@ -18,3 +19,6 @@ Route::post('/transfer', [TransferController::class, 'handle']);
 
 Route::get('/create', [CreateAccountController::class, 'view']);
 Route::post('/create', [CreateAccountController::class, 'handle']);
+
+Route::get('/deposit', [DepositController::class, 'view']);
+Route::post('/deposit', [DepositController::class, 'handle']);

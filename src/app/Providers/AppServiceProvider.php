e28e870acdd54bb0f226432ca3a14dcdf4e01417
sayuprc <bepdielibe+github@gmail.com>
2025-02-08 17:05:18 +0900

    DI コンテナの設定を追加

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use BankAccount\Applications\CreateAccount\CreateAccountInteractor;
+use BankAccount\Applications\Deposit\DepositInteractor;
 use BankAccount\Applications\List\ListInteractor;
 use BankAccount\Applications\Transfer\TransferInteractor;
+use BankAccount\Applications\Withdraw\WithdrawInteractor;
 use BankAccount\Domain\BankAccountRepositoryInterface;
 use BankAccount\Infrastructure\FileBankAccountRepository;
+use BankAccount\UseCases\CreateAccount\CreateAccountUseCaseInterface;
+use BankAccount\UseCases\Deposit\DepositUseCaseInterface;
 use BankAccount\UseCases\List\ListUseCaseInterface;
 use BankAccount\UseCases\Transfer\TransferUseCaseInterface;
+use BankAccount\UseCases\Withdraw\WithdrawUseCaseInterface;
 use Illuminate\Support\ServiceProvider;
 use Shared\DebugTransaction\NopTransaction;
 use Shared\Transaction\TransactionInterface;
@@ -25,6 +31,9 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->bind(ListUseCaseInterface::class, ListInteractor::class);
         $this->app->bind(TransferUseCaseInterface::class, TransferInteractor::class);
+        $this->app->bind(CreateAccountUseCaseInterface::class, CreateAccountInteractor::class);
+        $this->app->bind(DepositUseCaseInterface::class, DepositInteractor::class);
+        $this->app->bind(WithdrawUseCaseInterface::class, WithdrawInteractor::class);
 
         $this->app->bind(BankAccountRepositoryInterface::class, FileBankAccountRepository::class);
     }
